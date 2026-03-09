fb3b9e0327e59ad0cd0c56d8225d02d16174b109
Carlos Felipe Santos <ccfelipe08@gmail.com>
2026-03-09 13:27:09 -0300

    Estrutura das DTOs
    Criando pastas organizadas onde ficaram as classes DTOs(Data Transfer Object) de criação e de resposta

--- Hermes/obj/Debug/net10.0/Hermes.AssemblyInfo.cs
+++ Hermes/obj/Debug/net10.0/Hermes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f25ec4281a8567f1e9b2430085fdb6b9407b3fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f377cb45da11bf4fa616027e57ef6d79c54e95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
