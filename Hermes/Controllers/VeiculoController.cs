c60de103ce5d76197455133b633590292243125e
Carlos Felipe Santos <ccfelipe08@gmail.com>
2026-03-10 14:42:04 -0300

    CRUD Veiculos

--- Hermes/Controllers/VeiculoController.cs
+++ Hermes/Controllers/VeiculoController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 
 using Hermes.DTOs.Veiculo;
 using Hermes.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hermes.Controllers
 {
@@ -21,6 +22,23 @@ namespace Hermes.Controllers
             _mapper = mapper;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VeiculoDTO>>> Listar()
+        {
+            var veiculos = await _context.Veiculos.ToListAsync();
+            return Ok(_mapper.Map<List<VeiculoDTO>>(veiculos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VeiculoDTO>> Buscar(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo == null)
+                return NotFound();
+            
+            return Ok(_mapper.Map<VeiculoDTO>(veiculo));
+        }
+
         [HttpPost]
         public async Task<IActionResult> Criar(CriarVeiculo veiculoDto)
         {
@@ -30,6 +48,36 @@ namespace Hermes.Controllers
             await _context.SaveChangesAsync();
             
             return Ok(_mapper.Map<VeiculoDTO>(veiculo));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Atualizar(int id, CriarVeiculo dto)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+                return NotFound();
+
+            _mapper.Map(dto, veiculo);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletar(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+                return NotFound();
+
+            _context.Veiculos.Remove(veiculo);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
     }
--- Hermes/obj/Debug/net10.0/Hermes.AssemblyInfo.cs
+++ Hermes/obj/Debug/net10.0/Hermes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a45f733aa0db64ecece6e3e04a0865c43715fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7340a1d655230032471a54bdb88f58ad61c2f3a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
