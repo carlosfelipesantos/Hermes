57abf970d1556713d1b345fd0d10cda4faf1ee17
Carlos Felipe Santos <ccfelipe08@gmail.com>
2026-03-11 16:54:16 -0300

    Service Avaliacao

--- Hermes/Services/Implementations/AvaliacaoService.cs
+++ Hermes/Services/Implementations/AvaliacaoService.cs
@@ -1,6 +1,26 @@
-﻿namespace Hermes.Services.Implementations
+﻿using Hermes.Data;
+using Hermes.Entities;
+using Hermes.Services.Interfaces;
+
+namespace Hermes.Services.Implementations
 {
-    public class AvaliacaoService
+    public class AvaliacaoService : IAvaliacaoService
     {
+        private readonly HermesBD _context;
+
+        public AvaliacaoService(HermesBD context)
+        {
+            _context = context;
+        }
+
+        public async Task<Avaliacao> Criar(Avaliacao avaliacao)
+        {
+            avaliacao.DataAvaliacao = DateTime.Now;
+
+            _context.Avaliacoes.Add(avaliacao);
+            await _context.SaveChangesAsync();
+
+            return avaliacao;
+        }
     }
 }
--- Hermes/Services/Interfaces/IAvaliacaoService.cs
+++ Hermes/Services/Interfaces/IAvaliacaoService.cs
@@ -1,6 +1,9 @@
-﻿namespace Hermes.Services.Interfaces
+﻿using Hermes.Entities;
+
+namespace Hermes.Services.Interfaces
 {
     public interface IAvaliacaoService
     {
+        Task<Avaliacao> Criar(Avaliacao avaliacao);
     }
 }
