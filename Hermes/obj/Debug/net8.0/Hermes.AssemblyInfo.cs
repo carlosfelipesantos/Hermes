159f634e7f8e0f6c6588332e736e01e1361b8912
Carlos Felipe Santos <117771889+carlosfelipesantos@users.noreply.github.com>
2026-04-01 19:03:25 -0300

    .

--- Hermes/obj/Debug/net8.0/Hermes.AssemblyInfo.cs
+++ Hermes/obj/Debug/net8.0/Hermes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68643fe19b4caacd46d60a618bffe0505eb171e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b24d4a8bab5c87e75246515404b4337b2fe554")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
